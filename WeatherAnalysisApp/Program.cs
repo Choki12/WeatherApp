72ee9a374a531230b01539444a6d6eee42e590f8
Choki Tumane <BoitshokoTu@hollard.co.za>
2023-10-11 00:39:46 +0200

    Add project files.

---
+++ WeatherAnalysisApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WeatherAnalysis
+{
+    static class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Please enter the city name:");
+
+            var cityName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(cityName))
+            {
+                Console.WriteLine("City name cannot be empty.");
+            }
+
+            //business logic
+
+            Console.WriteLine("Weather report generated successfully");
+        }
+    }
+
+
+}
