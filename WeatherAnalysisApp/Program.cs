a89ea5e3dc2651a59cc90974cd4c3ea253a7f01e
Choki Tumane <BoitshokoTu@hollard.co.za>
2023-10-11 23:24:16 +0200

    Added logic to main WeatherApp main method to get city name and generate pdf

--- WeatherAnalysisApp/Program.cs
+++ WeatherAnalysisApp/Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
+using WeatherReportApp.Core.Interfaces;
+using WeatherReportApp.Core;
+using WeatherReportApp.Infrastructure.Services;
 
 namespace WeatherAnalysis
 {
     static class Program
     {
-        static void Main(string[] args) 
+        static async Task Main(string[] args) 
         {
-            Console.WriteLine("Please enter the city name:");
-
-            var cityName = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(cityName))
+            Console.WriteLine("Enter the name of the city for which you want to generate a weather report:");
+            string? cityName = Console.ReadLine();
+            if (string.IsNullOrEmpty(cityName))
             {
-                Console.WriteLine("City name cannot be empty.");
+                Console.WriteLine("No city name provided. Exiting...");
+                return;
             }
 
-            //business logic
+            // Create an instance of WeatherService and fetch weather data
+            IWeatherService weatherService = new WeatherService();
+            WeatherData weatherData = await weatherService.GetWeatherData(cityName);
 
-            Console.WriteLine("Weather report generated successfully");
+            // Create an instance of PdfReportService and generate PDF report
+            IPdfReportService pdfReportService = new PdfReportService();
+            
+
+            // Save the generated PDF
+            string outputPath = $"{AppDomain.CurrentDomain.BaseDirectory}\\{cityName}_WeatherReport.pdf";
+            var pdfDocument = pdfReportService.GenerateWeatherReport(weatherData, outputPath);
+            pdfDocument.Save(outputPath);
+            Console.WriteLine($"Weather report for {cityName} has been saved to: {outputPath}");
+
+            Console.ReadLine(); // Just to pause the console
         }
     }
 
